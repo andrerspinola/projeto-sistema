1b6218ae33feeadbca7ab2b91c028eee40f1b69a
andre <andrecastor828@gmail.com>
2025-08-29 11:45:37 -0300

    estilizando o nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,20 @@
 import {Link} from 'react-router-dom'
+import Logo from '../assets/imagem.png'
 
 const Nav = () => {
   return (
-    <header>
-        <nav>
-            <img src = "" alt = "logo"/>
-            <ul>
-                <li>
-                    <Link to = "/">Home</Link>
-                </li>
-                <li>
-                    <Link to = "/cliente">Cliente</Link>
-                </li>
-            </ul>
-        </nav>
+    <header className="bg-gray-800 text-white p-4 shadow-md">
+      <nav className="container mx-auto flex justify-between items-center">
+        <img src={Logo} alt="logo" className="h-10 w-auto"/>
+        <ul className="flex space-x-4">
+          <li className="list-none">
+            <Link to="/" className="hover:text-red-300 transition-colors duration-300" >Home</Link>
+          </li>
+          <li className="list-none">
+            <Link to="/cliente" className="hover:text-red-300 transition-colors duration-300">Cliente</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
